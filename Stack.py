91a5f8064a6e9caa8fd306bbafaae7d0bb1e5ab5
Priyanshu Shekhar Sinha <priyanshu.sinha@themathcompany.com>
2022-05-20 17:02:41 +0530

    added stack comments

--- Stack.py
+++ Stack.py
@@ -1,8 +1,16 @@
 class Stack:
+	"""
+	Class to create stack data structure
+	"""
 	def __init__(self):
 		self.stack = list()
 
 	def add(self, data):
+		"""
+		Adds new element to stack
+		:param data: new data element
+
+		"""
 		if data not in self.stack:
 			self.stack.append(data)
 			return True
@@ -10,9 +18,17 @@ class Stack:
 			return False
 
 	def peek(self):
+		"""
+		To view last element of stack
+		:return: last element of stack
+		"""
 		return self.stack[-1]
 
 	def remove(self):
+		"""
+		Removes the last element
+		:return: last element of the stack
+		"""
 		if len(self.stack) <= 0:
 			return 'Stack is empty'
 		return self.stack.pop()
